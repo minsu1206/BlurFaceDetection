999d7b8174c5aeb6751e138525abb55e0f9da401
Junyoung Park <79881119+junia3@users.noreply.github.com>
2022-08-10 12:32:42 +0900

    Add files via upload

--- FaceBlurring/models/mobilenet.py
+++ FaceBlurring/models/mobilenet.py
@@ -62,9 +62,9 @@ class InvertedResidual(nn.Module):
             return self.conv(x)
 
 
-class FaceMobileNet(nn.Module):
+class FaceMobileNetV2(nn.Module):
     def __init__(self, input_size=1024, width_mult=1.):
-        super(FaceMobileNet, self).__init__()
+        super(FaceMobileNetV2, self).__init__()
         input_channel = 32
         last_channel = 1280
         inverted_residual_setting = [
@@ -125,5 +125,60 @@ class FaceMobileNet(nn.Module):
                 m.weight.data.normal_(0, 0.01)
                 m.bias.data.zero_()
 
-def face_mobilenet(input_size=1024):
-    return FaceMobileNet(input_size)
+class FaceMobileNetV1(nn.Module):
+    def __init__(self, ch_in=3, n_classes=1):
+        super(FaceMobileNetV1, self).__init__()
+
+        def conv_bn(inp, oup, stride):
+            return nn.Sequential(
+                nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
+                nn.BatchNorm2d(oup),
+                nn.ReLU(inplace=True)
+            )
+
+        def conv_dw(inp, oup, stride):
+            return nn.Sequential(
+                # depthwise
+                nn.Conv2d(inp, inp, 3, stride, 1, groups=inp, bias=False),
+                nn.BatchNorm2d(inp),
+                nn.ReLU(inplace=True),
+
+                # pointwise
+                nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup),
+                nn.ReLU(inplace=True),
+            )
+
+        self.model = nn.Sequential(
+            conv_bn(ch_in, 32, 2),
+            conv_dw(32, 64, 1),
+            conv_dw(64, 128, 2),
+            conv_dw(128, 128, 1),
+            conv_dw(128, 256, 2),
+            conv_dw(256, 256, 1),
+            conv_dw(256, 512, 2),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 1024, 2),
+            conv_dw(1024, 1024, 1),
+            nn.AdaptiveAvgPool2d(1)
+        )
+        self.fc = nn.Linear(1024, n_classes)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.model(x)
+        x = x.view(-1, 1024)
+        x = self.fc(x)
+        x = self.sigmoid(x)
+        return x
+
+def face_mobilenet(input_size=1024, version=1):
+    if version == 2:
+        return FaceMobileNetV2(input_size)
+
+    elif version == 1:
+        return FaceMobileNetV1()
