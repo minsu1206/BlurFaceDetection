333b8fa086ea8c67209c7551a0b6657cb541ea3f
minsu1206 <minsumunsu@gmail.com>
2022-09-04 07:49:57 +0000

    blur_iterative

--- FaceBlurring/dataset/blur_iterative.py
+++ FaceBlurring/dataset/blur_iterative.py
@@ -1,6 +1,10 @@
-from blur import *
+import copy
+
+import numpy as np
+import torch
 from facenet_pytorch import MTCNN, InceptionResnetV1
-resnet = InceptionResnetV1(pretrained='vggface2').eval().cuda()
+
+from blur import *
 
 def iterative_blur(model, image, blur_method=''):
     # model = face recognition model
@@ -57,29 +61,91 @@ def iterative_blur(model, image, blur_method=''):
               return blur_image
             
             blur_image, blur_image_tensor = deblurGAN_blur_func(blur_image)
+
+def iterative_blur_n(model, image, n=3, dsize=(112, 112), blur_method_list=['deblurGAN', 'defocus'], device='cpu'):
+    '''
+    get n blur images list
+
+    Args:
+        image: clean image you want to get blurred images
+        n: iterative number
+        dsize: resize size of output image
+        blur_method_list
+
+    Returns:
+        clean_image: original Image
+        blur_image_list
+        cossim_list
+    '''
+    # [9/3 수정] -> process 최적화
+    # (1) redundant model build -> remove!
+    # (2) embedding -> batch forwarding!
+
+    clean_image = image
+    clean_img_tensor = torch.from_numpy(clean_image).permute(2, 0, 1).unsqueeze(0).float().to(device)
+    # clean_img_embed = model(clean_img_tensor).squeeze(0).cpu().detach()
+    blur_image = copy.deepcopy(image)
+    image_tensor_list, gen_image_list, cossim_list = [clean_img_tensor], [], []
+
+    for count in range(1, n+1):
+        blur_method = np.random.choice(blur_method_list)
+        if blur_method == 'defocus':
+            blur_image, blur_image_tensor = defocus_blur_func(blur_image)
+
+        elif blur_method == 'deblurGAN':
+            blur_image, blur_image_tensor = deblurGAN_blur_func(blur_image)
+
+        gen_blur_image = cv2.resize(blur_image, dsize=dsize, interpolation=cv2.INTER_AREA)
+        image_tensor_list.append(blur_image_tensor)
+        gen_image_list.append(gen_blur_image)
+
+    batch_imgs = torch.cat(image_tensor_list, dim=0)
+    # print(batch_imgs.shape) ok
+
+    cossim_list = cosine_similarity_batch(model, batch_imgs)
+    print(cossim_list)
+    clean_image = cv2.resize(clean_image, dsize=dsize, interpolation=cv2.INTER_AREA)
+    return clean_image, gen_image_list, cossim_list
     
 def defocus_blur_func(img):
     #input image (doesn't matter whether clean or blur)
     #return blurred image when the input is in.
-    blur_img, blur_deg = blurring(img, {'mean':50, 'var':20, 'dmin':0, 'dmax':10})
-    blur_img_tensor = torch.Tensor(blur_img).permute(2, 0, 1).unsqueeze(0).cuda()
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    blur_img, blur_deg = blurring(img, {'mean':25, 'var':10, 'dmin':0, 'dmax':50})
+    blur_img_tensor = torch.Tensor(blur_img).permute(2, 0, 1).unsqueeze(0).to(device)
     return blur_img, blur_img_tensor
 
 
 def deblurGAN_blur_func(img):
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
     trajectory = Trajectory({'canvas':64, 'iters':2000, 'max_len':60, 'expl':0.001}).fit()
     psf, degree = PSF(64, trajectory).fit()
     part_parameters = np.random.choice([1, 2, 3])
     clean_img, blur_img = BlurringImage(img, psf, part_parameters).blur_image()
-    blur_img_tensor = torch.Tensor(blur_img).permute(2, 0, 1).unsqueeze(0).cuda()
+    blur_img_tensor = torch.Tensor(blur_img).permute(2, 0, 1).unsqueeze(0).to(device)
     return blur_img, blur_img_tensor
 
 
-def cosine_similarity_func(clean_img, blur_img):
-    clean_img_tensor = torch.from_numpy(clean_img).permute(2, 0, 1).unsqueeze(0).float().cuda()
-    blur_img_tensor = torch.from_numpy(blur_img).permute(2, 0, 1).unsqueeze(0).float().cuda()
-    clean_img_embed = resnet(clean_img_tensor).squeeze(0).cpu().detach()
-    blur_img_embed = resnet(blur_img_tensor).squeeze(0).cpu().detach()
-    cossim = cos_sim(clean_img_embed, blur_img_embed)
+def cos_sim(emb1, emb2):
+	return np.dot(emb1, emb2)/(np.linalg.norm(emb1)*np.linalg.norm(emb2))
 
+def cosine_similarity_func(model, clean_embed, blur_img):
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    blur_img_tensor = torch.from_numpy(blur_img).permute(2, 0, 1).unsqueeze(0).float().to(device)
+    blur_img_embed = model(blur_img_tensor).squeeze(0).cpu().detach()
+    cossim = cos_sim(clean_embed, blur_img_embed)
     return cossim
+
+def cosine_similarity_batch(model, imgs):
+    img_embed = model(imgs)
+    clear_img_embed = img_embed[0, :]     # (F)
+    blur_imgs_embed = img_embed[1:, :]    # (B-1, F)
+
+    cossim_list = []
+    # Batch Cosine Similarity (Not on CPU. CUDA is faster. Detach follows CUDA matrix operation)
+
+    cossims = torch.nn.functional.cosine_similarity(clear_img_embed, blur_imgs_embed)
+    cossims = cossims.cpu().detach()
+    for cossim in cossims:
+        cossim_list.append(round(cossim.item(), 5))
+    return cossim_list
---
+++ FaceBlurring/dataset/create_blurimg_iterative.py
@@ -0,0 +1,119 @@
+import os
+import argparse
+from collections import defaultdict
+
+from tqdm import tqdm 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import cv2
+import torch
+from facenet_pytorch import MTCNN, InceptionResnetV1
+from blur_iterative import iterative_blur_n
+
+def get_img_path(root_path):
+    '''
+    get images' path from directory path
+
+    Args:
+        root path: Top directory path you want to obtain
+
+    Returns:
+        image path list
+    '''
+    img_path_list = []
+    for (root, _, files) in os.walk(root_path):
+        root = root.replace('\\', '/')
+        if len(files) != 0:
+            for img_path in files:
+                filename, file_extension = os.path.splitext(img_path)
+                if file_extension in ['.jpg', '.png']:
+                    img_path_list.append(root + '/' + img_path)
+    return img_path_list
+
+def save_iterative_blur_img(root_path, n, model, device):
+    '''
+    obtain blur images by applying the blur method several times
+    and save iterative blur images and cossim
+
+    Args:
+        root_path: Top directory path you want to obtain
+        n: iterative number
+    
+    '''
+    dsize=(112, 112)
+    dict_for_label = {'file_name' : [], '1-cossim' : []}
+
+    for (root, mid_list, files) in os.walk(root_path):
+        root = root.replace('\\', '/')
+        for mid in mid_list:
+            clean_save_path = f'../data/{dsize[0]}/clean/{mid}'
+            blur_save_path = f'../data/{dsize[0]}/blur/{mid}'
+            os.makedirs(clean_save_path, exist_ok=True)
+            os.makedirs(blur_save_path, exist_ok=True)
+
+    img_path_list = get_img_path(root_path)
+    #################################################
+    for img_path in tqdm(img_path_list):
+        img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+        clean_img, blur_img_list, cossim_list = iterative_blur_n(model, img, n, dsize, 
+        blur_method_list=['deblurGAN', 'defocus'], device=device)
+
+    #################################################
+        clean_img_path = img_path.replace('sample_root', f'{dsize[0]}')
+        cv2.imwrite(clean_img_path, clean_img)
+
+        for idx, blur_img in enumerate(blur_img_list):
+            blur_img_path = img_path.replace('sample_root', f'{dsize[0]}')
+            blur_img_path = blur_img_path.replace('clean', 'blur')
+            base_name = os.path.basename(img_path)
+            file_num, _ = os.path.splitext(base_name)
+            blur_img_path = blur_img_path.replace(file_num, file_num+'_'+str(idx+1))
+            dict_for_label['file_name'].append(blur_img_path)
+            dict_for_label['1-cossim'].append(1-cossim_list[idx])
+            cv2.imwrite(blur_img_path, blur_img)
+
+    save_dir = ".."+os.path.sep+os.path.join('data', f"{dsize[0]}", 'label')
+    os.makedirs(save_dir, exist_ok=True)
+    df = pd.DataFrame(dict_for_label)
+    df.to_csv(os.path.join(save_dir, "label.csv"))
+
+def draw_distribution(label_path):
+    '''
+    Draw x-axis: number of blur methods applied and y-axis: [1-cosine similarity]
+
+    Args:
+        label_path: label path containing blur label
+    '''
+    df = pd.read_csv(label_path)
+    cossim_dict = defaultdict(list)
+    cossim_list = []
+
+    for file_path, sim_label in zip(df['file_name'], df['1-cossim']):
+        key = int(file_path.split('_')[1][:-4]) # blur count
+        cossim_dict[key].append(sim_label)
+
+    for key in cossim_dict:
+        cossim_list.append(np.mean(np.array(cossim_dict[key])))
+    plt.plot(np.arange(1, len(cossim_list)+1), cossim_list)
+    plt.xlabel('iterative count')
+    plt.ylabel('1-cossim')
+    plt.show()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='This program creates iterative blur images.')
+    parser.add_argument('--path', type=str, default='../data/sample_root/clean')
+    parser.add_argument('--n', type=int, default=4, help='Maximum number of times to repeat blur')
+    args = parser.parse_args()
+
+    # [9/3] 수정
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model = InceptionResnetV1(pretrained='vggface2', device=device).eval()
+
+    # [9/3] Test : Batch Forwarding?
+    # dummy = torch.zeros((8, 3, 256, 256)).to(device)
+    # dummy_out = model(dummy)
+    # print(dummy_out.shape)    >> [8, 512]
+
+    save_iterative_blur_img(args.path, args.n, model=model, device=device)
+
