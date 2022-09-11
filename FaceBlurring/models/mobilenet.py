85b0bf1047a4f933323c13a9fbdb01eb729840da
minsu1206 <minsumunsu@gmail.com>
2022-09-11 14:04:46 +0000

    mobilenet

--- FaceBlurring/models/mobilenet.py
+++ FaceBlurring/models/mobilenet.py
@@ -5,13 +5,7 @@ class MobileNet (nn.Module):
     def __init__(self, num_classes):
         super().__init__()
         self.backbone = torch.hub.load('pytorch/vision:v0.10.0', 'mobilenet_v2', weights=True)
-        self.classifier = nn.Sequential(nn.Linear(1280, 128),
-                                        nn.BatchNorm1d(128),
-                                        nn.ReLU(inplace=True),
-                                        nn.Linear(128, 32),
-                                        nn.BatchNorm1d(32),
-                                        nn.ReLU(inplace=True),
-                                        nn.Linear(32, num_classes)
+        self.classifier = nn.Sequential(nn.Linear(1280, num_classes)
         )
 
     def forward(self, x):
