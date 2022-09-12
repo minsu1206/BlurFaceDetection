e2a50dd3e44800caf16addcf3e85727ed010b34f
minsu1206 <minsumunsu@gmail.com>
2022-09-12 03:53:40 +0000

    typo corrected

--- FaceBlurring/models/squeezenet.py
+++ FaceBlurring/models/squeezenet.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 def squeezenet1_1(num_classes=1):
     model = torch.hub.load('pytorch/vision:v0.10.0', 'squeezenet1_1', pretrained=True)
     model.classifier = nn.Sequential(
-        nn.Conv2d(512, n_classes, kernel_size=(1, 1), stride=(1, 1)),
+        nn.Conv2d(512, num_classes, kernel_size=(1, 1), stride=(1, 1)),
         nn.ReLU(inplace=True),
         nn.AdaptiveAvgPool2d(output_size=(1, 1)),
         nn.Sigmoid())
