7641f9e2b9514ff8c76fcbc93d1c4bd5e9bfb278
SungBeom-Choi <choisb3631@gmail.com>
2022-09-12 21:06:22 +0900

    Integrate tendency test file

--- FaceBlurring/experiments/tendency_test.py
+++ FaceBlurring/experiments/tendency_test.py
@@ -1,16 +1,18 @@
-from tqdm import tqdm
-import cv2, os
-import torch
-import numpy as np
-from facenet_pytorch import InceptionResnetV1
-import matplotlib.pyplot as plt
+import os
+import argparse
 
-# Get average distances from angle fixed samples
-def calculate_distances(img_dir, idx):
-    reference_img = cv2.imread(os.path.join(img_dir, 'fix_0.png'))
-    emb_clean = resnet(torch.Tensor(reference_img).permute(2, 0, 1).unsqueeze(0))
-    blurred_img = cv2.imread(os.path.join(img_dir, f'fix_{idx}.png'))
-    emb_blur = resnet(torch.Tensor(blurred_img).permute(2, 0, 1).unsqueeze(0))
+from tqdm import tqdm
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+from facenet_pytorch import InceptionResnetV1
+
+def calculate_distances(img_dir, idx, mode, model):
+    reference_img = cv2.imread(os.path.join(img_dir, f'{mode}_0.png'))
+    emb_clean = model(torch.Tensor(reference_img).permute(2, 0, 1).unsqueeze(0))
+    blurred_img = cv2.imread(os.path.join(img_dir, f'{mode}_{idx}.png'))
+    emb_blur = model(torch.Tensor(blurred_img).permute(2, 0, 1).unsqueeze(0))
     l1 = L1_distance(emb_clean, emb_blur)
     l2 = L2_distance(emb_clean, emb_blur)
     cossim = cos_sim(emb_clean.squeeze(0).detach().numpy(), emb_blur.squeeze(0).detach().numpy())
@@ -26,10 +28,10 @@ def L2_distance(emb1, emb2):
 def cos_sim(A, B):
     return np.dot(A, B)/(np.linalg.norm(A)*np.linalg.norm(B))
 
-if __name__ == '__main__':
+def plot_blur_sample_distance(mode='random'):
+    '''Get average distances from angle fix/random samples'''
     resnet = InceptionResnetV1(pretrained='vggface2').eval()
     path = './data/FFHQ_1024/clean/'
-
     look_upto = 0
     l1_mean, l2_mean, cos_mean = np.zeros(100), np.zeros(100), np.zeros(100)
 
@@ -38,7 +40,7 @@ if __name__ == '__main__':
             sample_path = os.path.join(path, subpath)
             look_upto += 1
             for i in tqdm(range(1, 101)):
-                _, l1, l2, cossim = calculate_distances(sample_path, i)
+                _, l1, l2, cossim = calculate_distances(sample_path, i, mode, resnet)
                 l1_mean[i-1] += l1
                 l2_mean[i-1] += l2
                 cos_mean[i-1] += cossim
@@ -55,4 +57,11 @@ if __name__ == '__main__':
     plt.plot(l2_mean, color='darkgreen', linewidth=2, label="Average L2 distance")
     plt.plot(cos_mean, color='darkblue', linewidth=2, label='Average Cosine similarity')
     plt.legend(fontsize=15)
-    plt.savefig("fix_L1L2COS.png")
+    plt.savefig(f"./results/distance test/{mode}_L1L2COS.png")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mode', type=str, default='random', help='Select type of data samples. It can be "fix" or "random"')
+    args = parser.parse_args()
+
+    plot_blur_sample_distance(args.mode)
